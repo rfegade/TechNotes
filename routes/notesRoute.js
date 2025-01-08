125493409d5933c15ac843d21c615be2f34173ac
Rashmi Fegade <fegaderashmi2@gmail.com>
2025-01-08 10:33:18 -0500

    update note Route to add API calls

--- routes/notesRoute.js
+++ routes/notesRoute.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-router.route("/").get().post().patch().delete();
+// import notes controller
+const notesController = require("../controllers/noteController");
+
+router
+  .route("/")
+  .get(notesController.getAllNotes)
+  .post(notesController.createNewNote)
+  .patch(notesController.updateNote)
+  .delete(notesController.deleteNote);
 
 module.exports = router;
